ea2d399c2301404bb34f90ff77df1d138a4ade00
Denis Dimchev <denis.dimchev@gmail.com>
2020-10-30 20:02:44 +0100

    Added Footer and React Scroll

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa'
+import {
+    FooterContainer, 
+    FooterWrap, 
+    FooterLinksContainer, 
+    FooterLinksWrapper, 
+    FooterLink, 
+    FooterLinkItems, 
+    FooterLinkTitle, 
+    SocialMedia, 
+    SocialMediaWrap, 
+    SocialLogo, 
+    SocialIcons, 
+    SocialIconLink,
+    WebsiteRights
+} from './FooterElements'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to='/signin'>How it works</FooterLink>
+                                <FooterLink to='/signin'>Testimonials</FooterLink>
+                                <FooterLink to='/signin'>Careers</FooterLink>
+                                <FooterLink to='/signin'>Investors</FooterLink>
+                                <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to='/signin'>How it works</FooterLink>
+                                <FooterLink to='/signin'>Testimonials</FooterLink>
+                                <FooterLink to='/signin'>Careers</FooterLink>
+                                <FooterLink to='/signin'>Investors</FooterLink>
+                                <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to='/signin'>How it works</FooterLink>
+                                <FooterLink to='/signin'>Testimonials</FooterLink>
+                                <FooterLink to='/signin'>Careers</FooterLink>
+                                <FooterLink to='/signin'>Investors</FooterLink>
+                                <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to='/signin'>How it works</FooterLink>
+                                <FooterLink to='/signin'>Testimonials</FooterLink>
+                                <FooterLink to='/signin'>Careers</FooterLink>
+                                <FooterLink to='/signin'>Investors</FooterLink>
+                                <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            futurn
+                        </SocialLogo>
+                        <WebsiteRights>Futurn © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label='Facebook'>
+                                <FaFacebook/>
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.twitter.com/" target="_blank" aria-label='Twitter'>
+                                <FaTwitter/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label='Instagram'>
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label='Linkedin'>
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label='YouTube'>
+                                <FaYoutube/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
